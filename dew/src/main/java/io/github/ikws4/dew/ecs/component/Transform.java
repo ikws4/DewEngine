db45a799248bdadcf8ede7623337c9e4d2884939
ikws4 <zhipingne@gmail.com>
2022-04-25 23:26:30 +0800

    refactor: Transform

--- dew/src/main/java/io/github/ikws4/dew/ecs/component/Transform.java
+++ dew/src/main/java/io/github/ikws4/dew/ecs/component/Transform.java
@@ -2,26 +2,19 @@ package io.github.ikws4.dew.ecs.component;
 
 import org.joml.Matrix4f;
 import org.joml.Vector2f;
+import org.joml.Vector3f;
 
 public class Transform {
-  public final Matrix4f transform;
+  private final Matrix4f transform;
 
-  public Transform(Vector2f position, Vector2f scale, float rotation) {
+  public Transform(Vector3f position, Vector2f scale, float rotation) {
     transform = new Matrix4f();
-    translate(position.x, position.y);
-    scale(scale.x, scale.y);
-    rotate(rotation);
+    transform.translate(position.x, position.y, 0)
+      .scale(scale.x, scale.y, 0)
+      .rotate(rotation, 0, 0, 1);
   }
-
-  public void translate(float x, float y) {
-    transform.translate(x, y, 0);
-  }
-
-  public void rotate(float angle) {
-    transform.rotate(angle, 0, 0, 1);
-  }
-
-  public void scale(float x, float y) {
-    transform.scale(x, y, 1);
+  
+  public void applyTo(Vector3f position) {
+    transform.transformPosition(position);
   }
 }
