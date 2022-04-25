87406d9bf807970d895700b7117e8761883ab06a
ikws4 <zhipingne@gmail.com>
2022-04-25 23:03:37 +0800

    feat: add Camera

--- dew/src/main/java/io/github/ikws4/dew/ecs/component/Camera.java
+++ dew/src/main/java/io/github/ikws4/dew/ecs/component/Camera.java
@@ -3,5 +3,33 @@ package io.github.ikws4.dew.ecs.component;
 import org.joml.Matrix4f;
 
 public class Camera {
-  public Matrix4f mvp;
+  private final Matrix4f projection;
+  private final Matrix4f view;
+  public final Matrix4f mvp;
+
+  public Camera(int width, int height) {
+    float hw = width / 2f;
+    float hh = height / 2f;
+
+    projection = new Matrix4f();
+    projection.ortho2D(-hw, hw, -hh, hh);
+
+    view = new Matrix4f();
+    view.lookAt(0, 0, 1, 0, 0, 0, 0, 1, 0);
+    
+    mvp = new Matrix4f();
+    projection.mul(view, mvp);
+  }
+
+  public void follow(float x, float y) {
+    view.lookAt(x, y, 1, x, y, 0, 0, 1, 0);
+  }
+
+  public void zoom(float factor) {
+    projection.scale(factor, factor, 1);
+  }
+
+  public void update() {
+    projection.mul(view, mvp);
+  }
 }
